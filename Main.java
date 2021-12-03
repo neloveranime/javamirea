d693c55958b402c388c1e86364c74c8482122b5f
neloveranime <75075697+neloveranime@users.noreply.github.com>
2021-12-03 12:54:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package prac1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Барон", "Чёрный", 6);
+
+        dog.getInfo();
+        System.out.println("\n");
+    }
+}
